3965bcfc9ea9ede4338f2d7d05f2b38339c055bb
Billy Hao <billyhao12@gmail.com>
2020-06-12 21:21:56 -0700

    HTML file now renders

--- Develop/app.js
+++ Develop/app.js
@@ -52,9 +52,13 @@ function askForManagerInfo() {
                 }
             ])
 
-        .then(( {name, id, email, officeNumber} ) => {
+        .then(({
+            name,
+            id,
+            email,
+            officeNumber
+        }) => {
             employees.push(new Manager(name, id, email, officeNumber));
-            console.log(employees);
             askForRole();
         });
 
@@ -68,16 +72,16 @@ function askForRole() {
 
     inquirer
         .prompt(
-            [
-                {
-                    type: "list",
-                    message: "What is your role?",
-                    choices: ["Engineer", "Intern"],
-                    name: "role"
-                }
-            ])
+            [{
+                type: "list",
+                message: "What is your role?",
+                choices: ["Engineer", "Intern"],
+                name: "role"
+            }])
 
-        .then(( {role} ) => {
+        .then(({
+            role
+        }) => {
             role === "Engineer" ? askForEngineerInfo() : askForInternInfo();
         });
 
@@ -101,9 +105,13 @@ function askForEngineerInfo() {
 
             ])
 
-        .then( ({name, id, email, github}) => {
-            employees.push( new Engineer(name, id, email, github) );
-            console.log(employees);
+        .then(({
+            name,
+            id,
+            email,
+            github
+        }) => {
+            employees.push(new Engineer(name, id, email, github));
             addAnother();
         });
 
@@ -126,9 +134,13 @@ function askForInternInfo() {
                 }
 
             ])
-        .then( ({name, id, email, school}) => {
+        .then(({
+            name,
+            id,
+            email,
+            school
+        }) => {
             employees.push(new Intern(name, id, email, school));
-            console.log(employees);
             addAnother();
         });
 }
@@ -137,32 +149,37 @@ function addAnother() {
 
     inquirer
         .prompt(
-            [
-                {
+            [{
                     type: "confirm",
                     message: "Add another team member?",
                     name: "addAnother"
                 }
 
             ])
-        .then( ({addAnother}) => {
-            if (addAnother) askForRole()
-            else console.log(employees);
+        .then(({
+            addAnother
+        }) => {
+            if (addAnother) {
+                askForRole();
+            } else {
+                output(render(employees));
+            }
         });
 }
 
+function output(teamProfile) {
+
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdir(OUTPUT_DIR, function (err) {
+            if (err) console.log(err);
+        });
+    };
+
+    fs.writeFile(outputPath, teamProfile, (err) => {
+        if (err) throw err;
+        console.log('HTML file successfully built!');
+    });
+
+}
+
 askForManagerInfo();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above to target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
