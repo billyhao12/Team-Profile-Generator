0179f91f2cc7c9269a9a527940a2b5639b26b84a
Billy Hao <billyhao12@gmail.com>
2020-06-12 20:05:01 -0700

    Refactored code using more functions and conditionals instead of the 'when' keyword

--- Develop/app.js
+++ Develop/app.js
@@ -6,87 +6,152 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const employees = [];
 
-const initQuestions = [
+employeeQuestions = [
+
     {
         type: "input",
-        message: "Enter your full name",
+        message: "Enter employee's full name",
         name: "name"
     },
 
     {
         type: "input",
-        message: "Enter your employee ID",
+        message: "Enter employee ID",
         name: "id"
     },
 
     {
-       type: "input",
-       message: "Enter your email",
-       name: "email"
-    },
-
-    {
-        type: "list",
-        message: "What is your role?",
-        choices: ["Manager", "Engineer", "Intern"],
-        name: "role"
-    },
-
-    {
         type: "input",
-        message: "Enter your office number",
-        when: function(answers) {
-            return (answers.role === "Manager")
-        },
-        name: "officeNumber"
-    },
-
-    {
-        type: "input",
-        message: "Enter your GitHub username",
-        when: function(answers) {
-            return (answers.role === "Engineer")
-        },
-        name: "github"
-    },
-
-    {
-        type: "input",
-        message: "Enter your school's name",
-        when: function(answers) {
-            return (answers.role === "Intern")
-        },
-        name: "school"
-    },
-
-    {
-        type: "confirm",
-        message: "Add another employee?",
-        name: "addAnother"
+        message: "Enter employee's email",
+        name: "email"
     }
+
 ];
 
-function init() {
+function askForManagerInfo() {
+
+    console.log("---------------------");
+    console.log("Enter manager info");
+    console.log("---------------------");
 
     inquirer
-    .prompt(initQuestions)
-    .then( response => {
-        
-        console.log(response);
+        .prompt(
+            [
+                ...employeeQuestions,
+                {
+                    type: "input",
+                    message: "Enter your office number",
+                    name: "officeNumber"
+                }
+            ])
 
-    });
+        .then(( {name, id, email, officeNumber} ) => {
+            employees.push(new Manager(name, id, email, officeNumber));
+            console.log(employees);
+            askForRole();
+        });
 
 }
 
-init();
+function askForRole() {
+
+    console.log("---------------------");
+    console.log("Adding a team member");
+    console.log("---------------------");
+
+    inquirer
+        .prompt(
+            [
+                {
+                    type: "list",
+                    message: "What is your role?",
+                    choices: ["Engineer", "Intern"],
+                    name: "role"
+                }
+            ])
+
+        .then(( {role} ) => {
+            role === "Engineer" ? askForEngineerInfo() : askForInternInfo();
+        });
+
+}
+
+function askForEngineerInfo() {
+
+    console.log("---------------------");
+    console.log("Enter engineer info");
+    console.log("---------------------");
+
+    inquirer
+        .prompt(
+            [
+                ...employeeQuestions,
+                {
+                    type: "input",
+                    message: "Enter your GitHub username",
+                    name: "github"
+                }
+
+            ])
+
+        .then( ({name, id, email, github}) => {
+            employees.push( new Engineer(name, id, email, github) );
+            console.log(employees);
+            addAnother();
+        });
+
+}
+
+function askForInternInfo() {
+
+    console.log("---------------------");
+    console.log("Enter intern info");
+    console.log("---------------------");
+
+    inquirer
+        .prompt(
+            [
+                ...employeeQuestions,
+                {
+                    type: "input",
+                    message: "Enter your school's name",
+                    name: "school"
+                }
+
+            ])
+        .then( ({name, id, email, school}) => {
+            employees.push(new Intern(name, id, email, school));
+            console.log(employees);
+            addAnother();
+        });
+}
+
+function addAnother() {
+
+    inquirer
+        .prompt(
+            [
+                {
+                    type: "confirm",
+                    message: "Add another team member?",
+                    name: "addAnother"
+                }
+
+            ])
+        .then( ({addAnother}) => {
+            if (addAnother) askForRole()
+            else console.log(employees);
+        });
+}
+
+askForManagerInfo();
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
